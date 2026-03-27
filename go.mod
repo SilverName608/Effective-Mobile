452739410e75c269bd6cf7cce7c70f91a2809b8d
Максим Кожекин <palach608@gmail.com>
2026-03-27 16:47:16 +0300

    Написал DI

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/pressly/goose/v3 v3.27.0
 	github.com/sirupsen/logrus v1.9.4
+	go.uber.org/fx v1.24.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -14,12 +16,16 @@ require (
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
 )
--- internal/di/app.go
+++ internal/di/app.go
@@ -1,1 +1,80 @@
 package di
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/pressly/goose/v3"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+
+	"github.com/SilverName608/Effective-Mobile/internal/api"
+	"github.com/SilverName608/Effective-Mobile/internal/config"
+	"github.com/SilverName608/Effective-Mobile/internal/database"
+	"github.com/SilverName608/Effective-Mobile/internal/repository"
+	"github.com/SilverName608/Effective-Mobile/internal/service"
+)
+
+func NewApp() *fx.App {
+	return fx.New(
+		fx.Provide(config.Load),
+		fx.Provide(database.NewPostgres),
+		fx.Provide(newLogger),
+
+		fx.Provide(fx.Annotate(
+			repository.NewSubscriptionRepository,
+			fx.As(new(repository.SubscriptionRepositoryI)),
+		)),
+
+		fx.Provide(fx.Annotate(
+			service.NewSubscriptionService,
+			fx.As(new(service.SubscriptionServiceI)),
+		)),
+
+		fx.Provide(api.NewSubscriptionHandler),
+		fx.Provide(api.NewRouter),
+
+		fx.Invoke(runServer),
+	)
+}
+
+func newLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetOutput(os.Stdout)
+	logger.SetLevel(logrus.InfoLevel)
+	return logger
+}
+
+func runServer(cfg *config.Config, router chi.Router, db *gorm.DB, log *logrus.Logger) {
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get sql.DB: %v", err)
+	}
+	if err := runMigrations(sqlDB); err != nil {
+		log.Fatalf("migrations failed: %v", err)
+	}
+
+	addr := fmt.Sprintf(":%s", cfg.App.Port)
+	log.Infof("server starting -> http://localhost%s", addr)
+
+	if err := http.ListenAndServe(addr, router); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+}
+
+func runMigrations(db *sql.DB) error {
+	if err := goose.SetDialect("postgres"); err != nil {
+		return err
+	}
+	if err := goose.Up(db, "migrations"); err != nil {
+		return err
+	}
+	log.Println("migrations applied")
+	return nil
+}
