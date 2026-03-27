775d0ebf841e79ad5a86d1b17041aab8c7a36efc
Максим Кожекин <palach608@gmail.com>
2026-03-27 15:44:48 +0300

    Написал Middleware

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,1 +1,26 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Middleware struct {
+	log *logrus.Logger
+}
+
+func NewMiddleware(log *logrus.Logger) *Middleware {
+	return &Middleware{log: log}
+}
+
+func (m *Middleware) Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.log.WithFields(logrus.Fields{
+			"method": r.Method,
+			"path":   r.URL.Path,
+		}).Info("incoming request")
+
+		next.ServeHTTP(w, r)
+	})
+}
